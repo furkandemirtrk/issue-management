f8cdebb99a256f5cfbe302be536b548b499c2f50
FURKAN DEMIRTURK <furkan-demirtrk@yandex.com>
2020-08-24 19:57:49 +0300

    swagger is created

--- src/main/java/com/angularspringboot/issuemanagement/api/IssueController.java
+++ src/main/java/com/angularspringboot/issuemanagement/api/IssueController.java
@@ -3,8 +3,9 @@ package com.angularspringboot.issuemanagement.api;
 
 import com.angularspringboot.issuemanagement.dto.IssueDto;
 import com.angularspringboot.issuemanagement.service.impl.IssueServiceImpl;
-import com.angularspringboot.issuemanagement.service.impl.ProjectServiceImpl;
 import com.angularspringboot.issuemanagement.util.ApiPaths;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping(ApiPaths.IssueCtrl.CTRL)
+@Api(value = ApiPaths.IssueCtrl.CTRL ,description = "Issue APIs")
 public class IssueController{
 
   private final IssueServiceImpl issueService;
@@ -22,22 +24,26 @@ public class IssueController{
 
 
   @GetMapping("/{id}")
+  @ApiOperation(value = "Get By Id Operation", response = IssueDto.class)
   public ResponseEntity<IssueDto> getById(@PathVariable(value = "id", required = true) Long id){
     IssueDto IssueDto = issueService.getById(id);
     return ResponseEntity.ok(IssueDto);
   }
 
   @PostMapping
+  @ApiOperation(value = "Create Operation", response = IssueDto.class)
   public ResponseEntity<IssueDto> createProject(@Valid @RequestBody IssueDto issueDto){
     return ResponseEntity.ok(issueService.save(issueDto));
   }
 
   @PutMapping("/{id}")
+  @ApiOperation(value = "Update Operation", response = IssueDto.class)
   public ResponseEntity<IssueDto> updateProject(@PathVariable(value = "id", required = true) Long id ,@Valid @RequestBody IssueDto issueDto){
     return ResponseEntity.ok(issueService.update(id,issueDto));
   }
 
   @DeleteMapping("/{id}")
+  @ApiOperation(value = "Delete Operation", response = Boolean.class)
   public ResponseEntity<Boolean> deleteProject(@PathVariable("id") Long id){
     return ResponseEntity.ok(issueService.delete(id));
   }
--- src/main/java/com/angularspringboot/issuemanagement/api/ProjectController.java
+++ src/main/java/com/angularspringboot/issuemanagement/api/ProjectController.java
@@ -1,10 +1,10 @@
 package com.angularspringboot.issuemanagement.api;
 
 import com.angularspringboot.issuemanagement.dto.ProjectDto;
-import com.angularspringboot.issuemanagement.entity.Project;
 import com.angularspringboot.issuemanagement.service.impl.ProjectServiceImpl;
 import com.angularspringboot.issuemanagement.util.ApiPaths;
-import org.springframework.http.HttpMethod;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +13,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping(ApiPaths.ProjectCtrl.CTRL)
+@Api(value = ApiPaths.ProjectCtrl.CTRL ,description = "Project APIs")
 public class ProjectController{
 
   private final ProjectServiceImpl projectServiceImpl;
@@ -22,22 +23,26 @@ public class ProjectController{
   }
 
   @GetMapping("/{id}")
+  @ApiOperation(value = "Get By Id Operation", response = ProjectDto.class)
   public ResponseEntity<ProjectDto> getById(@PathVariable(value = "id", required = true) Long id){
     ProjectDto projectDto = projectServiceImpl.getById(id);
     return ResponseEntity.ok(projectDto);
   }
 
   @PostMapping
+  @ApiOperation(value = "Create Operation", response = ProjectDto.class)
   public ResponseEntity<ProjectDto> createProject(@Valid @RequestBody ProjectDto projectDto){
     return ResponseEntity.ok(projectServiceImpl.save(projectDto));
   }
 
   @PutMapping("/{id}")
+  @ApiOperation(value = "Update Operation", response = ProjectDto.class)
   public ResponseEntity<ProjectDto> updateProject(@PathVariable(value = "id", required = true) Long id ,@Valid @RequestBody ProjectDto projectDto){
     return ResponseEntity.ok(projectServiceImpl.update(id,projectDto));
   }
 
   @DeleteMapping("/{id}")
+  @ApiOperation(value = "Delete Operation", response = Boolean.class)
   public ResponseEntity<Boolean> deleteProject(@PathVariable("id") Long id){
     return ResponseEntity.ok(projectServiceImpl.delete(id));
   }
